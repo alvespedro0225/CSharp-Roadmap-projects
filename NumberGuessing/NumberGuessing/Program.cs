0b7ca5afcc3ca1383d27a36db32e00d77cb25cea
Pedro <alvespedro0225@gmail.com>
2025-01-01 20:24:59 -0300

    refactored switch into lookup table

--- NumberGuessing/NumberGuessing/Program.cs
+++ NumberGuessing/NumberGuessing/Program.cs
@@ -17,23 +17,19 @@ public static class Program
                 Console.Write("Please enter a valid difficulty:");
                 continue;
             }
-            switch (difficulty.ToLower())
+            var difficultyTable = new Dictionary<string, short>
             {
-                case "easy":
-                    maxTries = 10;
-                    break;
-                
-                case "medium":
-                    maxTries = 5;
-                    break;
-                
-                case "hard":
-                    maxTries = 3;
-                    break;
-                
-                default:
-                    Console.Write("Please enter a valid difficulty: ");
-                    break;
+                { "easy", 10 },
+                { "medium", 5 },
+                { "hard", 3 }
+            };
+            try
+            {
+                maxTries = difficultyTable[difficulty.ToLower()];
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.Write("Please enter a valid difficulty:");
             }
         }
         
